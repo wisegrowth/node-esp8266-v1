6c139033ad72ad8c79c7e51f9731bcb2dd006909
Camilo Acuña <camilo.aku@gmail.com>
2017-06-20 04:49:31 -0400

    add sample env file

---
+++ sample-env.h
@@ -0,0 +1,14 @@
+#ifndef C_ENV_H
+#define C_ENV_H
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "wifi_password"
+
+#define AWS_ENDPOINT "aws_endpoint"
+#define AWS_KEY "aws_key"
+#define AWS_SECRET "aws_secret"
+#define AWS_REGION "aws_region"
+#define PORT port
+#define DEVICE_ID "device_id"
+
+#endif
